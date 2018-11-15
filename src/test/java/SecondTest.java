1d4998684b831bf135d495a13b43413b9fee763d
koresh_2000@mail.ru <and19korgrsu>
2018-11-15 17:10:44 +0300

    12

--- src/test/java/SecondTest.java
+++ src/test/java/SecondTest.java
@@ -5,7 +5,7 @@ public class SecondTest extends WebDriverSettings{
     @Test
     public void selenoidFirst() throws Exception {
         driver.get("http://www.yahoo.by");
-
+        System.out.println(selenoidUrl+"/video/"+getSessionId()+".mp4");
 
     }
 }
--- src/test/java/WebDriverSettings.java
+++ src/test/java/WebDriverSettings.java
@@ -1,6 +1,7 @@
 import com.google.common.io.Files;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -26,8 +27,8 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 public class WebDriverSettings {
     public WebDriver driver;
     //public ChromeDriver driver;
-   String selenoidUrlWdHub = "http://192.168.1.201:4444/wd/hub/";
-
+    String selenoidUrlWdHub = "http://192.168.1.201:4444/wd/hub/";
+    String selenoidUrl = "http://192.168.1.201:4444";
     @BeforeTest
     public void setUp() throws MalformedURLException {
         /**
@@ -54,6 +55,7 @@ public class WebDriverSettings {
         }
 
         driver.manage().window().setSize(new Dimension(1920,1080));
+        System.out.println(selenoidUrl+"/video/"+getSessionId()+".mp4");
         /**
          * локальный хромдрайвер
          */
@@ -71,56 +73,40 @@ public class WebDriverSettings {
 
 
     @AfterTest
-    public void endUp() throws MalformedURLException {
-        attachAllureVideo(getSessionId());
+    public void endUp() throws MalformedURLException, InterruptedException {
+
        /* String selenoidUrl = "http://192.168.1.201:4444";
         URL videoUrl = new URL( selenoidUrl + "/video/" + sessionId + ".mp4");
         System.out.println(videoUrl);*/
+        String sessionNumber = getSessionId();
         saveScreenshotPNG(driver);
         driver.quit();
+        Thread.sleep(2000);
+
+        attachAllureVideo(sessionNumber);
 
     }
 
-    @Attachment(value = "Page screenshot", type = "image/png")
-    public byte[] saveScreenshotPNG (WebDriver driver) {
-        return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
-    }
-
-    private static String selenoidUrl = "http://192.168.1.201:4444";
-    public void attachAllureVideo(String sessionId) {
+  public void attachAllureVideo(String sessionId) {
         try {
-            URL videoUrl = new URL(selenoidUrl + "/video/" + sessionId + ".mp4");
-            InputStream is = null;
-            Thread.sleep(1000);
-            for (int i = 0; i < 10; i++) {
-                try {
-                    is = videoUrl.openStream();
-                    i = 10;
-                } catch (FileNotFoundException e) {
-                    Thread.sleep(1000);
-                }
-            }
-            Allure.addAttachment("Video", "video/mp4", is, "mp4");
-        } catch (Exception e) {
-            System.out.println("attachAllureVideo");
-            e.printStackTrace();
-        }
-    }
- /* public void attachAllureVideo(String sessionId) {
-
-        try {
-            String selenoidUrl = "http://192.168.1.201:4444";
             URL videoUrl = new URL( selenoidUrl + "/video/" + sessionId + ".mp4");
-
             InputStream is = getSelenoidVideo(videoUrl);
             Allure.addAttachment("Video", "video/mp4", is, "mp4");
-
         } catch (Exception e) {
             System.out.println("attachAllureVideo");
             e.printStackTrace();
         }
-    } */
+    }
 
+ public String getSessionId(){
+     SessionId session = ((RemoteWebDriver)driver).getSessionId();
+     String sessionIdGet = session.toString();
+     return sessionIdGet;
+ }
+
+ /*    public static String getSessionId() {
+       return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+     }*/
     public  InputStream getSelenoidVideo(URL url) {
         int lastSize = 0;
         for (int i = 0; i < 20; i++) {
@@ -130,7 +116,7 @@ public class WebDriverSettings {
                 System.out.println(i);
                 if (size > lastSize || size == 19) {
                     lastSize = size;
-                    //Thread.sleep(1000);
+                    Thread.sleep(1000);
                     continue;
                 }
                 return url.openStream();
@@ -143,19 +129,9 @@ public class WebDriverSettings {
         return null;
     }
 
-    public  String getSessionId(){
-        SessionId session = ((RemoteWebDriver)driver).getSessionId();
-        String sessionIdGet = session.toString();
-        return sessionIdGet;
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshotPNG (WebDriver driver) {
+        return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
     }
-
-   /* public static String getSessionId() {
-
-        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
-    }*/
-
-
-
-
 
 }
