52c7da0060270551bcd6bb096216e7a61b124916
koresh_2000@mail.ru <and19korgrsu>
2018-11-15 11:44:03 +0300

    1

---
+++ src/test/java/SecondTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class SecondTest extends WebDriverSettings{
+
+    @Test
+    public void selenoidFirst() throws Exception {
+        driver.get("http://www.yahoo.by");
+
+    }
+}
